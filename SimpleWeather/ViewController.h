31b85601c8b3448f71a541b02935527ebea77e78
Michael Davidson <raistlin969@yahoo.com>
2014-10-17 11:52:07 -0400

    Initial commit

---
+++ SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by Michael Davidson on 10/17/14.
+//  Copyright (c) 2014 Michael Davidson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Michael Davidson on 10/17/14.
+//  Copyright (c) 2014 Michael Davidson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
