4d718615c6ce31406f6bf299b5a4f163ef585f6b
Michael <mdavidson@cala.fsu.edu>
2014-10-17 17:35:06 -0400

    added images

--- SimpleWeather/WXViewController.m
+++ SimpleWeather/WXViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "WXViewController.h"
+#import <LBBlurredImage/UIImageView+LBBlurredImage.h>
 
 @interface WXViewController ()
+
+@property (strong, nonatomic)UIImageView *backgroundImageView;
 
 @end
 
@@ -17,6 +20,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    self.view.backgroundColor = [UIColor redColor];
+    UIImage *background = [UIImage imageNamed:@"bg"];
+    self.backgroundImageView = [[UIImageView alloc] initWithImage:background];
+    self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [self.view addSubview:self.backgroundImageView];
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +37,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    CGRect bounds = self.view.bounds;
+
+    self.backgroundImageView.frame = bounds;
+}
 @end
